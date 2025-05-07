5569edec140f1fd71783aa09772ce72b73828d6c
reetnayan_gitlocal <reetnayan16@gmail.com>
2025-05-07 19:23:27 +0530

    refined sender and receiver

--- serverent1.py
+++ serverent1.py
@@ -1,19 +1,36 @@
 import os
 import socket
 
+def parse_filename(file_name:str)->list:
+    """
+    This function takes a file name and separates its name from the extension.
+    """
+    name_data = file_name.split('.') # Split filename on '.'
+
+    extension = name_data[ len(name_data) - 1 ]
+    name = '.'.join( name_data[ 0:len(name_data)-1 ] )
+
+    return [name, extension]
+
+
+    
+
+
 def send(addr:str, port:int, fileName:str):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((addr, port))
 
-    file_name = fileName
-    file_size = os.path.getsize(file_name)
+    file_name = parse_filename(fileName)[0]
+    file_ext = parse_filename(fileName)[1]
 
-    name_to_send = file_name.split('.')[0]
+    complete_file_name = file_name+'.'+file_ext
 
-    client.send(f"{name_to_send}<NAME_END>".encode())
+    file_size = os.path.getsize(complete_file_name)
+
+    client.send(f"{file_name}_received.{file_ext}<NAME_END>".encode())
     client.send(f"{file_size}<SIZE_END>".encode())
 
-    with open(file_name, "rb") as f:
+    with open(complete_file_name, "rb") as f:
         while (chunk := f.read(1024)):
             client.send(chunk)
         client.send("<FILE_END>".encode())
@@ -23,5 +40,5 @@ def send(addr:str, port:int, fileName:str):
 
 if __name__=="__main__":
     print("Sending...")
-    send('localhost', 9999, 'image.jpg')
+    send('localhost', 9999, "image.jpg")
     print("Done.")
--- serverent2.py
+++ serverent2.py
@@ -1,40 +1,42 @@
 import socket
 import tqdm
 
-server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9999))
-server.listen(1)
+def receive(addr:str, port:int, max_connections:int):
+    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((addr, port))
+    server.listen(max_connections)
 
-sender, addr = server.accept()
+    sender, addr = server.accept()
 
-buffer = b""
-while b"<SIZE_END>" not in buffer:
-    buffer += sender.recv(1024)
+    buffer = b""
+    while b"<SIZE_END>" not in buffer:
+        buffer += sender.recv(1024)
 
-file_name_encoded, remaining_data = buffer.split(b"<NAME_END>")
-file_size_encoded, remaining_data = remaining_data.split(b"<SIZE_END>")
+    file_name_encoded, remaining_data = buffer.split(b"<NAME_END>")
+    file_size_encoded, remaining_data = remaining_data.split(b"<SIZE_END>")
 
-file_name = file_name_encoded.decode()+"_recieved.jpg"
-file_size = int(file_size_encoded.decode())
+    file_name = file_name_encoded.decode()
+    file_size = int(file_size_encoded.decode())
 
-print(f"[FILE] {file_name}")
-print(f"[FILE SIZE] {file_size} bytes")
-print("\n--RECIEVING--\n")
+    print(f"[FILE] {file_name}")
+    print(f"[FILE SIZE] {file_size} bytes")
+    print("\n--RECIEVING--\n")
 
-progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(file_size))
+    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(file_size))
 
-with open(file_name, "wb") as f:
-    f.write(remaining_data)
-    bytes_recv = len(remaining_data)
+    with open(file_name, "wb") as f:
+        f.write(remaining_data)
+        bytes_recv = len(remaining_data)
 
-    while bytes_recv < file_size:
-        chunk = sender.recv(min(4096, file_size-bytes_recv))
-        if not chunk: 
-            break
-        f.write(chunk)
-        bytes_recv+=len(chunk)
-        progress.update(len(chunk))
-    
-sender.close()
-server.close()
-
+        while bytes_recv < file_size:
+            chunk = sender.recv(min(4096, file_size-bytes_recv))
+            if not chunk: 
+                break
+            f.write(chunk)
+            bytes_recv+=len(chunk)
+            progress.update(len(chunk))
+        
+    sender.close()
+    server.close()
+if __name__=="__main__":
+    receive('localhost', 9999, 1)
